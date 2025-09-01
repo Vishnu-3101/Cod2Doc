26af9567ba544e18e5ea252495a64aa983526beb
vishnu <vishnugadige2003@gmail.com>
2025-09-01 16:24:49 +0530

    Updated loader.py with functions

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import sys
 import logging
 import os
 from utils.build_graph import BuildGraph
+from utils.loader import retrieve_with_dependencies, get_doc
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
 
 # Setup logging
 logging.basicConfig(
@@ -23,3 +28,37 @@ if not os.path.exists(dependency_graph_path):
 
 else:
     print("Dependency graph already created. Using it for further processing....")
+
+
+docs, data = get_doc(dependency_graph_path)
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-l6-v2")
+
+vectorstore = FAISS.from_documents(docs, embeddings)
+embedding_retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+# BM25 retriever
+bm25_retriever = BM25Retriever.from_documents(docs)
+bm25_retriever.k = 2
+
+# Hybrid retriever (weighted ensemble)
+hybrid_retriever = EnsembleRetriever(
+    retrievers=[embedding_retriever, bm25_retriever],
+    weights=[0.3, 0.7] 
+)
+
+# Example
+query_code = "def backward():"
+results,expanded_results = retrieve_with_dependencies(query_code, hybrid_retriever, data)
+
+
+for result in results:
+    # print(result.metadata['score'])
+    print(result.page_content)
+    print("------------------------------------")
+
+# print(expanded_results)
+
+for res in expanded_results:
+    print(res["id"], "=> depends on", res["depends_on"])
+    print("------------------------------------")
--- utils/loader.py
+++ utils/loader.py
@@ -1,32 +1,28 @@
 import json
 from langchain.schema import Document
-
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
 from typing import List
 from langchain_core.runnables import chain
-from langchain_ollama import OllamaEmbeddings
-from langchain_community.retrievers import BM25Retriever
-from langchain.retrievers import EnsembleRetriever
 
 
-with open("output/dependency_graph.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
+def get_doc(output_path):
+    with open(output_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
 
-docs = []
-for comp_id, comp in data.items():
-    docs.append(
-        Document(
-            page_content=comp["source_code"],  # searchable content
-            metadata={
-                "id": comp["id"],
-                "component_type": comp["component_type"],
-                "depends_on": comp["depends_on"],
-                "file_path": comp["file_path"],
-                "relative_path": comp["relative_path"],
-            }
+    docs = []
+    for comp_id, comp in data.items():
+        docs.append(
+            Document(
+                page_content=comp["source_code"],  # searchable content
+                metadata={
+                    "id": comp["id"],
+                    "component_type": comp["component_type"],
+                    "depends_on": comp["depends_on"],
+                    "file_path": comp["file_path"],
+                    "relative_path": comp["relative_path"],
+                }
+            )
         )
-    )
+    return docs,data
 
 
 def retrieve_with_dependencies(query, retriever, data):
@@ -48,46 +44,3 @@ def retrieve_with_dependencies(query, retriever, data):
     
     return results,expanded
 
-
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-l6-v2")
-
-# embeddings = OllamaEmbeddings(
-#     model="qwen3:0.6b",
-# )
-
-vectorstore = FAISS.from_documents(docs, embeddings)
-embedding_retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
-
-# BM25 retriever
-bm25_retriever = BM25Retriever.from_documents(docs)
-bm25_retriever.k = 2
-
-# Hybrid retriever (weighted ensemble)
-hybrid_retriever = EnsembleRetriever(
-    retrievers=[embedding_retriever, bm25_retriever],
-    weights=[0.3, 0.7] 
-)
-
-# @chain
-# def retriever(query: str) -> List[Document]:
-#     docs, scores = zip(*vectorstore.similarity_search_with_score(query,k=4))
-#     for doc, score in zip(docs, scores):
-#         doc.metadata["score"] = score
-
-#     best_doc = sorted(docs, key=lambda d: d.metadata["score"],reverse=True)
-#     return best_doc
-
-# Example
-query_code = "def backward():"
-results,expanded_results = retrieve_with_dependencies(query_code, hybrid_retriever, data)
-# for res in expanded_results:
-#     print(res["id"], "=> depends on", res["depends_on"])
-
-# print(results)
-
-for result in results:
-    # print(result.metadata['score'])
-    print(result.page_content)
-    print("------------------------------------")
-
-# print(len(docs))
