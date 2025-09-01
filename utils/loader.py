841ed90490b03de5693a1db22db3d4a5666ab8c4
vishnu <vishnugadige2003@gmail.com>
2025-09-01 15:27:38 +0530

    Retrive code with Faiss+BM25

--- utils/loader.py
+++ utils/loader.py
@@ -5,6 +5,9 @@ from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from typing import List
 from langchain_core.runnables import chain
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
 
 
 with open("output/dependency_graph.json", "r", encoding="utf-8") as f:
@@ -47,28 +50,44 @@ def retrieve_with_dependencies(query, retriever, data):
 
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-l6-v2")
+
+# embeddings = OllamaEmbeddings(
+#     model="qwen3:0.6b",
+# )
+
 vectorstore = FAISS.from_documents(docs, embeddings)
-retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 1})
+embedding_retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
-@chain
-def retriever(query: str) -> List[Document]:
-    docs, scores = zip(*vectorstore.similarity_search_with_score(query,k=4))
-    for doc, score in zip(docs, scores):
-        doc.metadata["score"] = score
+# BM25 retriever
+bm25_retriever = BM25Retriever.from_documents(docs)
+bm25_retriever.k = 2
 
-    best_doc = sorted(docs, key=lambda d: d.metadata["score"],reverse=True)
-    return best_doc
+# Hybrid retriever (weighted ensemble)
+hybrid_retriever = EnsembleRetriever(
+    retrievers=[embedding_retriever, bm25_retriever],
+    weights=[0.3, 0.7] 
+)
+
+# @chain
+# def retriever(query: str) -> List[Document]:
+#     docs, scores = zip(*vectorstore.similarity_search_with_score(query,k=4))
+#     for doc, score in zip(docs, scores):
+#         doc.metadata["score"] = score
+
+#     best_doc = sorted(docs, key=lambda d: d.metadata["score"],reverse=True)
+#     return best_doc
 
 # Example
-query_code = "back = _backward()"
-results,expanded_results = retrieve_with_dependencies(query_code, retriever, data)
+query_code = "def backward():"
+results,expanded_results = retrieve_with_dependencies(query_code, hybrid_retriever, data)
 # for res in expanded_results:
 #     print(res["id"], "=> depends on", res["depends_on"])
 
 # print(results)
 
 for result in results:
-    print(result.metadata['score'])
+    # print(result.metadata['score'])
     print(result.page_content)
+    print("------------------------------------")
 
 # print(len(docs))
